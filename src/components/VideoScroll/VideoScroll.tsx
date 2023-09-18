df240250c19a60c3c8c9c3baf5d2cf336e99627e
Uri Elias <urieliasaks@hotmail.com>
2023-09-18 13:32:10 -0600

    playing mobile

--- src/components/VideoScroll/VideoScroll.tsx
+++ src/components/VideoScroll/VideoScroll.tsx
@@ -1,36 +1,79 @@
 import "./VideoScroll.css";
 import { throttle } from "lodash";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { VideoScrollProps } from "./VideoScroll.types";
 
 const VideoScroll = (props: VideoScrollProps) => {
     const { video, width, lengthScroll } = props;
     const videoRef = useRef<HTMLVideoElement>(null);
     const scrollContainerRef = useRef<HTMLDivElement>(null);
+    const [scrolling, setScrolling] = useState(false);
+
+    const handleScroll = throttle(() => {
+        window.requestAnimationFrame(() => {
+            const video = videoRef.current;
+            const scrollContainer = scrollContainerRef.current;
+            
+            if (!video || !scrollContainer) return;
+
+            const maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+            const scrollFraction = scrollContainer.scrollLeft / maxScroll;
+            const videoDuration = video.duration;
+            const targetTime = scrollFraction * videoDuration;
+
+            video.currentTime = targetTime;
+        });
+    }, 50);
 
     useEffect(() => {
-        const video = videoRef.current;
+        const scrollContainer = scrollContainerRef.current;
+        let startTouchX = 0;
+        let startScrollLeft = 0;
+
+        if (!scrollContainer) return;
+
+        const handleTouchStart = (e: TouchEvent) => {
+            startTouchX = e.touches[0].clientX;
+            startScrollLeft = scrollContainer.scrollLeft;
+            setScrolling(true);
+        };
+
+        const handleTouchMove = (e: TouchEvent) => {
+            if (scrolling) {
+                const touchX = e.touches[0].clientX;
+                const deltaX = touchX - startTouchX;
+                scrollContainer.scrollLeft = startScrollLeft - deltaX;
+                e.preventDefault();
+                handleScroll();
+            }
+        };
+
+        const handleTouchEnd = () => {
+            setScrolling(false);
+        };
+
+        scrollContainer.addEventListener("touchstart", handleTouchStart);
+        scrollContainer.addEventListener("touchmove", handleTouchMove);
+        scrollContainer.addEventListener("touchend", handleTouchEnd);
+
+        return () => {
+            scrollContainer.removeEventListener("touchstart", handleTouchStart);
+            scrollContainer.removeEventListener("touchmove", handleTouchMove);
+            scrollContainer.removeEventListener("touchend", handleTouchEnd);
+        };
+    }, [scrolling, handleScroll]);
+
+    useEffect(() => {
         const scrollContainer = scrollContainerRef.current;
 
-        if (!scrollContainer || !video) return;
-
-        const handleScroll = throttle(() => {
-            window.requestAnimationFrame(() => {
-                const maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
-                const scrollFraction = scrollContainer.scrollLeft / maxScroll;
-                const videoDuration = video.duration;
-                const targetTime = scrollFraction * videoDuration;
-
-                video.currentTime = targetTime;
-            });
-        }, 50);
+        if (!scrollContainer) return;
 
         scrollContainer.addEventListener("scroll", handleScroll);
 
         return () => {
             scrollContainer.removeEventListener("scroll", handleScroll);
         };
-    }, []);
+    }, [handleScroll]);
 
     useEffect(() => {
         const video = videoRef.current;
@@ -66,7 +109,7 @@ const VideoScroll = (props: VideoScrollProps) => {
                     }}
                 ></div>
             </div>
-            <video ref={videoRef} width="100%" height="auto" preload="auto" playsInline>
+            <video ref={videoRef} width="100%" height="auto" preload="auto" playsInline muted>
                 <source src={video} type="video/mp4" />
             </video>
         </div>
